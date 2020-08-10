e1841872917efabd92120d544909d99e1d0d363d
ugglr <carl.igelstrom@gmail.com>
2020-08-10 11:37:45 +0800

    basic filtering working.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,42 +1,60 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout/Layout"
-import { Container, Row, Col } from "react-bootstrap"
 import MyJumbo from "../components/myJumbo/MyJumbo"
 import ProjectCard from "../components/portProjectCard/ProjectCard"
 
 import { PROJECTS } from "../content/Projects"
 
+import styles from "./projects.module.css"
 import "./index.css"
 
 /*
 ######### PROJECT OBJECTS TEMPLATE################
 import projectPicture from ""
 const projectProject = {
-  title: "",
-  date: "",
-  decription: "",
-  sourceURL: "",
-  hostedURL: "",
 }
 #####################################################
 */
 
-export default () => (
-  <div className="App">
-    <Layout>
-      {/*########### MyJumbo PROPS ##########
+export default () => {
+  const [projects, setProjects] = useState([])
+  const [filter, setFilter] = useState(null)
+  const [filters, setFilters] = useState([])
+
+  const loadProjects = () => {
+    const filteredProjects = PROJECTS.filter(project => {
+      if (filter === null) {
+        console.log("filter null")
+        return true
+      }
+      return project.tech.includes(filter)
+    })
+    setProjects(filteredProjects)
+  }
+
+  useEffect(() => {
+    console.log("loading projects into state")
+    loadProjects()
+    console.log("State projects", projects)
+  }, [])
+
+  return (
+    <div className="App">
+      <Layout>
+        {/*########### MyJumbo PROPS ##########
     title: The title of the jumbotron
     body: The body of the Jumbotron
     */}
-      <MyJumbo
-        title={"Projects Portfolio"}
-        body={
-          "I think the best way of learning is by getting down to coding and build. I really appreciate feedback on anything that I have built!"
-        }
-      />
-      <hr />
-      <Container style={{ marginTop: "2.5rem" }}>
-        {/*/Props: 
+        <MyJumbo
+          title={"Projects Portfolio"}
+          body={
+            "I think the best way of learning is by getting down to coding and build. I really appreciate feedback on anything that I have built!"
+          }
+        />
+        <hr />
+        <div className={styles.projects_container}>
+          <div className={styles.filter_container}></div>
+          {/*/Props: 
       //imageSrcPath: the path to the image used 
       //title: The title of the card/App 
       //date: The date of the card
@@ -45,40 +63,37 @@ export default () => (
       //leftButtonText: changes the left button text
       //hostedURL: URL to the hosted version of the app
       //rightButtonText: changes the right button text*/}
-        {PROJECTS.map(
-          ({
-            img,
-            title,
-            date,
-            description,
-            leftButtonText,
-            hostedURL,
-            rightButtonText,
-            sourceURL,
-            tech,
-            tags,
-          }) => {
-            return (
-              <Row>
-                <Col>
-                  <ProjectCard
-                    imageSrcPath={img}
-                    title={title}
-                    date={date}
-                    description={description}
-                    sourceURL={sourceURL}
-                    leftButtonText={leftButtonText}
-                    hostedURL={hostedURL}
-                    rightButtonText={rightButtonText}
-                    tech={tech}
-                    tags={tags}
-                  />
-                </Col>
-              </Row>
-            )
-          }
-        )}
-      </Container>
-    </Layout>
-  </div>
-)
+          {projects.map(
+            ({
+              img,
+              title,
+              date,
+              description,
+              leftButtonText,
+              hostedURL,
+              rightButtonText,
+              sourceURL,
+              tech,
+              tags,
+            }) => {
+              return (
+                <ProjectCard
+                  imageSrcPath={img}
+                  title={title}
+                  date={date}
+                  description={description}
+                  sourceURL={sourceURL}
+                  leftButtonText={leftButtonText}
+                  hostedURL={hostedURL}
+                  rightButtonText={rightButtonText}
+                  tech={tech}
+                  tags={tags}
+                />
+              )
+            }
+          )}
+        </div>
+      </Layout>
+    </div>
+  )
+}
